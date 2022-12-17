9598a92a9f94ab883ba3cba0271c7ef820faa729
balaji.sigamani <balaji.sigamani@olacabs.com>
2022-12-17 21:04:44 +0530

    add redis db client

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"gitlab.com/idoko/rediboard/db"
+	"log"
+	// "net/http"
 )
 
+var (
+	ListenAddr = "localhost:8080"
+	RedisAddr = "localhost:6379"
+ )
+ func main() {
+	database, err := db.NewDatabase(RedisAddr)
+	if err != nil {
+	   log.Fatalf("Failed to connect to redis: %s", err.Error())
+	}
+ 
+	router := initRouter(database)
+	router.Run(ListenAddr)
+ }
 
-func main(){
-	fmt.Println("Hello World")
-}
+
+ func initRouter(database *db.Database) *gin.Engine {
+	r := gin.Default()
+	return r
+ }
